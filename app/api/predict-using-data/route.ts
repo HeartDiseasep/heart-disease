fdb444cedadcc07b555cd0d901d1f41bdbc8a51b
AdityaM2906 <adityameshram126@gmail.com>
2024-01-22 18:38:33 +0530

    updatiing config

--- app/api/predict-using-data/route.ts
+++ app/api/predict-using-data/route.ts
@@ -2,8 +2,6 @@ import { runScript } from "@/app/api/predict-using-data/process-spawner";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log(body);
-
   const process: any = await runScript(body);
   return Response.json(JSON.parse(process));
 }
--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config, options) => {
-  //   config.module.rules.push({
-  //     test: /\.py$/,
-  //     type: "asset/resource",
-  //     generator: {
-  //       filename: "static/chunks/[name].py",
-  //     },
-  //   });
-  //   return config;
-  // },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.py$/,
+      type: "asset/resource",
+      generator: {
+        filename: "scripts/[name].py",
+      },
+    });
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
